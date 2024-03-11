8470e888a398183e05e41479bccd8d30e45b9008
Fernando Acácio <162220102+FernandoSchtz@users.noreply.github.com>
2024-03-11 19:31:29 -0300

    Add files via upload

---
+++ exercicio2.java
@@ -0,0 +1,14 @@
+public class exercicio2 {
+
+        public static void main(String[] args) {
+            // declaração de variável inteira
+            int cpf_funcionario;
+            cpf_funcionario = "06869200904";
+
+            int cod_funcionario = 500;
+            
+            int idade = 20, num_filhos = 2;
+
+
+            System.out.println(cpf_funcionario,num_filhos,idade);
+}
---
+++ exercicio3.java
@@ -0,0 +1,16 @@
+public class exercicio3 {
+
+
+    public static void main(String[] args) {
+        
+       int Terra = 15, Marte = 20, Jupter = 30;
+        int Planetas = (Terra + Jupter + Marte)/3;
+
+        System.out.println(Planetas);
+
+
+
+    
+    }
+    
+}
